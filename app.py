b577e96c6780a110942736236a97bc658beebd09
VictorVanO <20253@ecam.be>
2025-10-01 14:01:32 +0200

    Updated docker-compose and added nginx

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Distributed Systems Project 2025"
 
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
